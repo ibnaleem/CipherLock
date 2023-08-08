0735949b14305843b507c3f58d3ceb6a6c3c57b9
ibnaleem <ibnaleem@outlook.com>
2023-08-08 21:57:44 +0100

    create encryption function

--- cmd/main.py
+++ cmd/main.py
@@ -21,3 +21,17 @@ ascii_art = '''
 
 key = Fernet.generate_key()
 cipher_suite = Fernet(key)
+
+encrypted_items = []
+
+def encrypt_item(path):
+  with open(path, "rb") as file:
+    data = file.read()
+    encrypted_data = cipher_suite.encrypt(data)
+    
+    encrypted_path = f"encrypted_{os.path.basename(path)}"
+    with open(encrypted_path, "wb") as encrypted_file:
+      encrypted_file.write(encrypted_data)
+    
+    encrypted_items.append(encrypted_path)
+    return encrypted_path
